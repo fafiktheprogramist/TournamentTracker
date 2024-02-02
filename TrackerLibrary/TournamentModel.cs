9c8b0a82ce5c0037c42e45f7b3fd3f18e4d4af46
fafiktheprogramist <glowaczra@gmail.com>
2024-02-02 15:01:15 +0100

    changes files tree to get rid of the error

---
+++ TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,14 @@
+namespace TrackerLibrary
+{
+    public class TournamentModel
+    {
+        public int Id { get; set; }
+        public string TournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
+        public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
+        public List<List<MatchupModel>> Rounds { get; set; } = new List<List<MatchupModel>> ();
+
+
+    }
+}
